aec16189ad045e22f167f47f7c5fa853a761dc00
playceab <lijian@playcarb.com>
2017-11-21 18:50:33 +0800

    f

--- web/js/demo.js
+++ web/js/demo.js
@@ -553,7 +553,7 @@ function success_topbar(type,data,msg){
     var html = '';
     html +='<div class="alert alert-'+val+' light alert-dismissable">';
     html +='   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button><i class="fa fa-info pr10"></i>';
-    html +='    <strong>'+data+'</strong>'+message;
+    html +='    <strong>'+data+'</strong>'+msg;
     html +=' </div>';
     $('#alert_topbar').html(html);
     $("#alert_topbar").slideToggle(3000);
